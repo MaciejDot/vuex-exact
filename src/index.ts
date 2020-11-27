6caa04bb3b9c18c37653bfdfcb737e5fbfc6a2d2
Maciek_dot <maciejd0@op.pl>
2020-11-27 22:41:25 +0100

    n

--- src/index.ts
+++ src/index.ts
@@ -1,78 +1,46 @@
-import { convertTypeAcquisitionFromJson, isSwitchStatement } from 'typescript';
-import Vuex, { Store, StoreOptions, MutationTree, CommitOptions, ModuleTree, ActionTree, DispatchOptions, mapActions, GetterTree, Module } from 'vuex'
+import { StoreOptions, Module, CommitOptions, ModuleTree } from 'vuex'
 type PropType<TObj, TProp extends keyof TObj> = TObj[TProp]
-type UnsafePropType<TObj extends Record<string, any>, TProp extends string> = TObj[TProp ]
+type UnsafePropType<TObj extends Record<string, any>, TProp extends string> = TObj[TProp];
 
-var storeOptions = {
-    mutations: {
-        name: (state: any, payload: string) => { },
-        name2: (state: any, p: number) => { }
-    },
-    actions: {
-        ac: (n: { state: any, commit: any }, payload: string) => Promise.resolve(payload)
-    }
-    , getters: {
-        get: (state) => 2
-    },
-    modules: {
-        mod: {
-            getters: {
-                nom: () => 32
-            }
-        }
-    }
-}// as StoreOptions<any>;
- 
-type Op = typeof storeOptions;
-type UnpackMutations<State, Mutations extends MutationTree<State>> =
-    <T extends keyof Mutations>
-        (type: T,
-        payload: Parameters<
-            PropType<Mutations, T> extends (...args: any) => any ?
-            PropType<Mutations, T>
-            : (state: any, payload: any) => any>[1],
-        options?: CommitOptions) => void;
+type UnpackMutation<Prefix extends string, State, R, Impl extends StoreOptions<State> | Module<State, R>>
+    = <T extends keyof PropType<Impl, "mutations">>(type:
+        T extends string ?
+        "namespaced" extends keyof Impl ?
+        PropType<Impl, "namespaced"> extends true ? T : `${Prefix}${T}`
+     : T: "",
+    payload: Parameters <
+        PropType < PropType < Impl, "mutations" >, T > extends (...args: any) => any ?
+            PropType<PropType<Impl, "mutations">, T>
+    : (state: any, payload: any) => any > [1],
+                options ?: CommitOptions) => void;
 
-type UnpackActions<State, Actions extends ActionTree<State, any>> =
-    <T extends keyof Actions>
-        (type: T,
-        payload: Parameters<
-            PropType<Actions, T> extends (...args: any) => any ?
-            PropType<Actions, T>
-            : (state: any, payload: any) => any>[1],
-        options?: DispatchOptions) => ReturnType<PropType<Actions, T> extends (...args: any) => any ?
-            PropType<Actions, T> : () => Promise<any>>;
 
-type UnpackGetters<State, Getters extends GetterTree<State, any>> =
-{
-        readonly [T in keyof Getters]: ReturnType<PropType<Getters, T>>
-}
 
-type UnpackState<State, Modules extends ModuleTree<State>> = State & {
-    [T in keyof Modules] : UnpackState<PropType<PropType<Modules, T>, "state">,
-        NonNullable<PropType<PropType<Modules, T>, "modules">>
+
+
+
+
+type UnpackMutationsModule<T extends keyof PropType<Impl, "modules">, Prefix extends string, State, R, Impl extends StoreOptions<State> | Module<State, R>>
+    = UnpackMutation<Prefix, State, R, Impl> | UnpackMutations<
+        T extends string ?
+        Prefix extends ""? `${T}/` : `${Prefix}/${T}/` : "",
+            UnsafePropType < PropType < PropType < Impl, "modules" >, T >, "state" >,
+            any,
+            PropType < PropType < Impl, "modules" >, T >>
+
+type UnpackMutations < Prefix extends string, State, R, Impl extends StoreOptions<State> | Module < State, R >> =
+                ReturnType << T extends keyof PropType < Impl, "modules" >> () => UnpackMutationsModule < T, Prefix, State, R, Impl >>
+
+
+type UnpackState < State, Modules extends ModuleTree < State >> = State & {
+                [T in keyof Modules] : UnpackState<PropType< PropType < Modules, T >, "state" >,
+                NonNullable < PropType < PropType < Modules, T >, "modules" >>
     >
 }
 
-export type UnpackStore<State, StoreOptionsImpl extends StoreOptions<State>> =  {
-    
-    readonly commit:
-    UnpackMutations<State, NonNullable<PropType<StoreOptionsImpl, "mutations">>>,
-    readonly dispatch:
-    UnpackActions<State, NonNullable<PropType<StoreOptionsImpl, "actions">>>,
-    readonly getters:
-    UnpackGetters<State, NonNullable<PropType<StoreOptionsImpl, "getters">>>
-    state: UnpackState<State, NonNullable<PropType<StoreOptionsImpl, "modules">>>
-} 
+export type UnpackStore<State, StoreOptionsImpl extends StoreOptions<State>> = {
 
-export type Un<StoreOption>{
-    state: PropType<StoreOption, "state">
-}
-
-
-
-
-
-//
-var deapth: UnpackStore<string, Op>;
-deapth.commit("")
+                    readonly commit:
+                    UnpackMutations<"", State, any, StoreOptionsImpl>,
+                    state: UnpackState<State, NonNullable<PropType<StoreOptionsImpl, "modules">>>
+                } 
