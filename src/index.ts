8986af2ca5ecd5e652444e26bf9835949c338c7f
Maciek_dot <maciejd0@op.pl>
2020-11-28 11:58:20 +0100

    pack

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,44 @@
-import { StoreOptions, Module, CommitOptions, ModuleTree } from 'vuex'
+import { StoreOptions, Module, CommitOptions, ModuleTree, Store, DispatchOptions } from 'vuex'
 
 type PropType<TObj, TProp extends keyof TObj> = TObj[TProp]
 type UnsafePropType<TObj extends Record<string, any>, TProp extends string> = TObj[TProp];
 
+//actions unpack
+type UnpackActionGroup<Prefix extends string, State, R, Impl extends StoreOptions<State> | Module<State, R>>=
+{
+    [T in keyof PropType<Impl, "actions"> as T extends string ?
+    "namespaced" extends keyof Impl ?
+    PropType<Impl, "namespaced"> extends true ? T : `${Prefix}${T}`
+: T: ""]:
+    { payload: Parameters<
+    PropType<PropType<Impl, "actions">, T> extends (...args: any) => any ?
+    PropType<PropType<Impl, "actions">, T>
+    : (state: any, payload: any) => any>[1],
+        returnType: ReturnType<
+        PropType<PropType<Impl, "actions">, T> extends (...args: any) => any ?
+        PropType<PropType<Impl, "actions">, T>
+        : (state: any, payload: any) => any>
+    } 
+}    
 
+type UnpackActionModule<T extends keyof PropType<Impl, "modules">, Prefix extends string, State, R, Impl extends StoreOptions<State> | Module<State, R>>
+    =
+    PropType<Impl, "modules"> extends undefined ?
+    UnpackActionGroup<Prefix, State, R, Impl>:
+    keyof PropType<Impl, "modules"> extends never ?
+
+    UnpackActionGroup<Prefix, State, R, Impl> :
+    UnpackActionGroup<Prefix, State, R, Impl> & UnpackActions<
+        T extends string ?
+        Prefix extends "" ? `${T}/` : `${Prefix}/${T}/` : "",
+        UnsafePropType<PropType<PropType<Impl, "modules">, T>, "state">,
+        any,
+        PropType<PropType<Impl, "modules">, T>>
+
+type UnpackActions<Prefix extends string, State, R, Impl extends StoreOptions<State> | Module<State, R>> =
+    UnpackActionModule<keyof PropType<Impl, "modules">, Prefix, State, R, Impl>
+
+//mutations unpack
 type UnpackMutationGroup<Prefix extends string, State, R, Impl extends StoreOptions<State> | Module<State, R>>=
 {
     [T in keyof PropType<Impl, "mutations"> as T extends string ?
@@ -33,61 +68,53 @@ type UnpackMutationsModule<T extends keyof PropType<Impl, "modules">, Prefix ext
 type UnpackMutations<Prefix extends string, State, R, Impl extends StoreOptions<State> | Module<State, R>> =
     UnpackMutationsModule<keyof PropType<Impl, "modules">, Prefix, State, R, Impl>
 
+//getters unpack
+type UnpackGettersGroup<Prefix extends string, State, R, Impl extends StoreOptions<State> | Module<State, R>>=
+{
+    [T in keyof PropType<Impl, "getters"> as T extends string ?
+    "namespaced" extends keyof Impl ?
+    PropType<Impl, "namespaced"> extends true ? T : `${Prefix}${T}`
+: T: ""]:
+    ReturnType<PropType<PropType<Impl, "getters">, T> extends (...args : any) => any ? PropType<PropType<Impl, "getters">, T> : ()=> any>
+}    
+
+type UnpackGettersModule<T extends keyof PropType<Impl, "modules">, Prefix extends string, State, R, Impl extends StoreOptions<State> | Module<State, R>>
+    =
+    PropType<Impl, "modules"> extends undefined ?
+    UnpackGettersGroup<Prefix, State, R, Impl>:
+    keyof PropType<Impl, "modules"> extends never ?
+
+    UnpackGettersGroup<Prefix, State, R, Impl> :
+    UnpackGettersGroup<Prefix, State, R, Impl> & UnpackGetters<
+        T extends string ?
+        Prefix extends "" ? `${T}/` : `${Prefix}/${T}/` : "",
+        UnsafePropType<PropType<PropType<Impl, "modules">, T>, "state">,
+        any,
+        PropType<PropType<Impl, "modules">, T>>
+
+type UnpackGetters<Prefix extends string, State, R, Impl extends StoreOptions<State> | Module<State, R>> =
+UnpackGettersModule<keyof PropType<Impl, "modules">, Prefix, State, R, Impl>
+
 type UnpackState<State, Modules extends ModuleTree<State>> = State & {
     [T in keyof Modules]: UnpackState<PropType<PropType<Modules, T>, "state">,
         NonNullable<PropType<PropType<Modules, T>, "modules">>
     >
 }
 
-export type UnpackStore<State, StoreOptionsImpl extends StoreOptions<State>> = {
-
-    readonly commit<T extends keyof UnpackMutations<"", State, any, StoreOptionsImpl>>
+export type UnpackStore<State, StoreOptionsImpl extends StoreOptions<State>> = 
+Omit<Store<State>, "commit" | "dispatch" | "getters" | "state" >
+& {
+    commit<T extends keyof UnpackMutations<"", State, any, StoreOptionsImpl>>
         (
             type : T , 
             payload : UnpackMutations<"", State, any, StoreOptionsImpl>[T],
             commitOptions?: CommitOptions) : void
+    dispatch<T extends keyof UnpackActions<"",State, any, StoreOptionsImpl>>
+        (
+            type : T , 
+            payload : PropType<UnpackActions<"", State, any, StoreOptionsImpl>[T], "payload">,
+            dispatchOptions?: DispatchOptions
+        ): PropType<UnpackActions<"", State, any, StoreOptionsImpl>[T], "returnType">
+    readonly getters : UnpackGetters<"",State, any, StoreOptionsImpl>
     state: UnpackState<State, NonNullable<PropType<StoreOptionsImpl, "modules">>>
 }
-
-
-
-const store = {
-    state: {
-        lan: ""
-    },
-    mutations: {
-        mutter: (state: any, pay: string) => { },
-        molec: (state: any) => {}
-    }
-    modules: {
-           ber: {
-               namespaced: true,
-               state: {
-                   lan  : ""
-               },
-               mutations: {
-                   mutter : (state : any, pay: number) => {}
-               }
-           }
-    }
-}
-
-const val : Record<"a", string> & Record<"b", number>
-
-
-
-
-const pack: UnpackStore<typeof store.state, typeof store>;
-pack.commit("molec",)
-
-//{}Store.dispatch(" erfferf",{})
-
-
-
-
-
-
-
-
-
-
